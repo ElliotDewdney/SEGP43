990f385a5c2ae137c3138b3531d09dbccbfd63c0
ElliotDewdney <60190829+ElliotDewdney@users.noreply.github.com>
2023-03-01 14:37:38 +0000

    added reflection

--- src/main/java/uk/ac/soton/ecs/model/obstruction/ObstructionSingleton.java
+++ src/main/java/uk/ac/soton/ecs/model/obstruction/ObstructionSingleton.java
@@ -1,20 +1,49 @@
 package uk.ac.soton.ecs.model.obstruction;
 
+import  uk.ac.soton.ecs.model.obstruction.obstructions.*;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.reflections.Reflections;
+import java.lang.reflect.InvocationTargetException;
+
 public class ObstructionSingleton {
 	
 	private static ObstructionSingleton instance = null;
 	
 	public static ObstructionSingleton getInstance() {
 		if(instance == null) {
-			instance = new ObstructionSingleton();
+			try {
+				instance = new ObstructionSingleton();
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		return instance;
 	}
 	
-	public static class FactoryRecord{
+	private HashMap<String, ObstructionFactory> factories = new HashMap<>();
+	
+	private ObstructionSingleton() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		Reflections reflections  = new Reflections(PackageInfo.class.getPackageName());
+		Set<Class<?>> tempFactories = reflections.getTypesAnnotatedWith(ObstructionFactoryType.class);
 		
+		for(Class<?> tempFactory : tempFactories) {
+			ObstructionFactory factory = (ObstructionFactory) tempFactory.getConstructor().newInstance();
+			String name = tempFactory.getAnnotationsByType(ObstructionFactoryType.class)[0].obstructionName();
+			factories.put(name, factory);
+		}
 		
-		
+	}
+	
+	public Set<String> getFactoryNames() {
+		return factories.keySet();
+	}
+	
+	public ObstructionFactory getFactoryByName(String name) {
+		return factories.get(name);
 	}
 }
---
+++ src/main/java/uk/ac/soton/ecs/model/obstruction/obstructions/PackageInfo.java
@@ -0,0 +1,5 @@
+package uk.ac.soton.ecs.model.obstruction.obstructions;
+
+public class PackageInfo {
+
+}
